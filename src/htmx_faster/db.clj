c504b893880690e1ec667108dc65da8e5e3e9142
Maksim Nikalayevich <max5.7maxi@gmail.com>
2025-03-05 01:50:38 +0400

    remove unused comment

--- src/htmx_faster/db.clj
+++ src/htmx_faster/db.clj
@@ -1,5 +1,6 @@
 (ns htmx-faster.db
-  (:require [pg.core :as pg]))
+  (:require
+    [pg.core :as pg]))
 
 (def config
   {:host "db"
@@ -8,17 +9,6 @@
    :password "postgres"
    :database "mydb"})
 
-;(def config
-;  {:host "localhost"
-;   :port 5432
-;   :user "postgres"
-;   :password "123123"
-;   :database "htmxfaster"})
-
 (def conn
   (try (pg/connect config)
        (catch Exception _ nil)))
-
-(comment
-
-  (pg/query conn "select * from collections"))
